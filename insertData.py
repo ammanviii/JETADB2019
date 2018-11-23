2735db34193898fab068c5d43c1a591639b21ddd
ammanviii <ammanuelamsal@gmail.com>
2018-11-23 16:20:20 -0500

    Amman: I added Suppliers to insertData and created a backup for my current datatbase

--- insertData.py
+++ insertData.py
@@ -16,14 +16,17 @@ customers_last_names=[]
 
 #This list contains random customerID numbers without repetition shhuffled, print to see it
 accounts_cid = random.sample(list(range(1,101)), k=100)
-fakePasswords=[]
+fake_Passwords=[]
 
 #This is a dictionary to store each email with it's associated customerID
 customer_id_email={}
 
+fake_companies=[]
+
 filename1='firstNames.txt'
 filename2='lastNames.txt'
 filename3='fakePasswords.txt'
+filename4='fakeCompanies.txt'
 
 with open (filename1) as fin:
     for line in fin:
@@ -36,7 +39,12 @@ with open (filename2) as fin:
 
 with open (filename3) as fin:
     for line in fin:
-        fakePasswords.append(line.strip())
+        fake_Passwords.append(line.strip())
+
+with open (filename4) as fin:
+    for line in fin:
+        fake_companies.append(line.strip())
+
 
 
 def insert_employee(Fname, Lname):
@@ -218,6 +226,36 @@ def get_email():
         if conn is not None:
             conn.close()
 
+def insert_suppliers(SupplierName):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO Suppliers(SupplierName)
+             VALUES(%s) RETURNING SupplierID;"""
+    conn = None
+    SupplierID = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (SupplierName,))
+        # get the generated id back
+        SupplierID = cur.fetchone()[0]
+        print("The SupplierID is: "+f'{SupplierID}')
+        print(" ")
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return SupplierID
 
 
 
@@ -226,21 +264,21 @@ if __name__ == '__main__':
     #generates 100 fake employees
     # UNCOMMENT TO INSERT AND CHANGE range to range(100)
     print("The Employees are:","\n")
-    for x in range(100):
+    for x in range(1):
     
         first_employee = random.choice(employees_first_names)
         last_employee = random.choice(employees_last_names)
 
         # insert_employee(first_employee, first_employee)
-        print(f'{first_employee} {last_employee}\n')
-        print("#"*50)
-        print(" ")
+        # print(f'{first_employee} {last_employee}\n')
+        # print("#"*50)
+        # print(" ")
 
 
     #generates 100 fake customers
     # UNCOMMENT TO INSERT AND CHANGE range to range(100)
     print("The Customers are:","\n")
-    for x in range(100):
+    for x in range(1):
         first_customer = random.choice(customers_first_names)
         last_customer = random.choice(customers_last_names)
 
@@ -257,13 +295,13 @@ if __name__ == '__main__':
         address = f'{street_num} {street} St.,'
         
         # insert_customers(first_customer, last_customer, phone_string, email, address, city, state, zip_string)
-        print(f'{first_customer}\n{last_customer}\n{phone_string}\n{email}\n{address}\n{city}\n{state}\n{zip_string}\n')
-        print("#"*50)
-        print(" ")
+        # print(f'{first_customer}\n{last_customer}\n{phone_string}\n{email}\n{address}\n{city}\n{state}\n{zip_string}\n')
+        # print("#"*50)
+        # print(" ")
 
      ##generates fake Orders:
     print("The Orders are","\n")
-    for x in range(100):
+    for x in range(1):
         cid = random.randint(1,100)
         yr = random.randint(2016,2018)
         mo = random.randint(1,12)
@@ -273,14 +311,14 @@ if __name__ == '__main__':
         trackingNo = random.randint(10000000,99999999)
 
         # insert_orders(cid,orderDate,trackingNo, carrierid)
-        print ("The order is: ", f'{cid} {yr} {mo} {day} {orderDate} {carrierid} {trackingNo}\n')
-        print("#"*50)
-        print(" ")
+        # print ("The order is: ", f'{cid} {yr} {mo} {day} {orderDate} {carrierid} {trackingNo}\n')
+        # print("#"*50)
+        # print(" ")
 
 
     ##generates fake tickets:
     print("The Tickets are:","\n")
-    for x in range(100):
+    for x in range(1):
         cid = random.randint(1,100)
         oid = random.randint(1,100)
         eid = random.randint(1,100)
@@ -291,9 +329,9 @@ if __name__ == '__main__':
         supportMessage = f'I have {random.choice(ticketBank1)} about {random.choice(ticketBank2)}'
 
         # insert_tickets(cid,supportMessage,oid,eid,ticketdate)
-        print("The ticket is: ",supportMessage,"\n")
-        print("#"*50)
-        print(" ")
+        # print("The ticket is: ",supportMessage,"\n")
+        # print("#"*50)
+        # print(" ")
     
     ##generates fake accounts:
     #We run get_email() so we can get the emails
@@ -301,12 +339,25 @@ if __name__ == '__main__':
     print(accounts_cid, "\n")
     print("The Accounts are:\n")
 
-    for x in range(100): 
+    for x in range(1): 
         cid = accounts_cid[x]
         email = customer_id_email.get(cid)
-        password = random.choice(fakePasswords) 
+        password = random.choice(fake_Passwords) 
 
         # insert_accounts(email,password,cid)
-        print(f'{cid} {password} {email}\n')
+        # print(f'{cid} {password} {email}\n')
+        # print("#"*50)
+        # print(" ")
+    
+
+     #generates 100 fake companies
+    # UNCOMMENT TO INSERT AND CHANGE range to range(100)
+    print("The Companies are:","\n")
+    for x in range(100):
+        randomCompanies = random.sample(fake_companies, k=100)
+        suppliers = random.choice(randomCompanies)
+
+        # insert_suppliers(suppliers)
+        print(f'{suppliers}\n')
         print("#"*50)
         print(" ")
